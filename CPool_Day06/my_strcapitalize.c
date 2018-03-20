230b53d04289f1c99ab5d703e4bd3714100b21fb
17150012 <17150012@bjtu.edu.cn>
2018-03-20 22:10:51 +0800

    first commit

--- CPool_Day06/my_strcapitalize.c
+++ CPool_Day06/my_strcapitalize.c
@@ -4,10 +4,10 @@ char *my_strcapitalize(char *str)
 {
 	int i = 0;
 	str = my_strlowcase(str);
+	if(str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
 	while(str[i])
 	{
-		if(i == 0 && str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
 		if((str[i] >= 7 && str[i] <= 13) || (str[i] >= 32 && str[i] <= 47) || (str[i] >= 58 && str[i] <= 64))
 		{
 			if(str[i+1] >= 'a' && str[i+1] <= 'z')
