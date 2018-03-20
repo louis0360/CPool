e4a9822cf13e6ea81650e3fef1dde4077ddd8ac4
17150012 <17150012@bjtu.edu.cn>
2018-03-20 21:32:59 +0800

    first commit

--- CPool_Day06/my_strcapitalize.c
+++ CPool_Day06/my_strcapitalize.c
@@ -8,7 +8,7 @@ char *my_strcapitalize(char *str)
 	{
 		if(i == 0 && str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
-		if(str[i] == ' ' || str[i] == '+' || str[i] == '-')
+		if((str[i] >= 32 && str[i] <= 47) || (str[i] >= 58 && str[i] <= 64))
 		{
 			if(str[i+1] >= 'a' && str[i+1] <= 'z')
 				str[i+1] -= 32;
